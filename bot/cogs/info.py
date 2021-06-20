6e482eb7901397ebed81c62d51bdba22d7aa7e45
Chris A <1drturtle@gmail.com>
2021-06-20 13:05:28 -0400

    ping and uptime

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 
 from utils.embeds import DefaultEmbed
+import pendulum
 
 
 class Info(commands.Cog):
@@ -24,6 +25,30 @@ class Info(commands.Cog):
             value='Dr Turtle#1771'
         )
 
+        return await ctx.send(embed=embed)
+
+    @commands.command(name='ping')
+    @commands.cooldown(5, 5, type=commands.BucketType.user)
+    async def ping(self, ctx):
+        """Shows the latency of the bot."""
+        embed = DefaultEmbed(ctx)
+        # add bot ping
+        embed.title = f'{self.bot.user.name} Ping'
+        embed.add_field(name='Bot Ping', value=f'```fix\n{round(self.bot.latency * 1000)} ms\n```')
+        # time sending message
+        websocket = pendulum.now()
+        msg = await ctx.send(embed=embed)
+        websocket = pendulum.now() - websocket
+        # edit embed for discord ping and send
+        embed.add_field(name='Discord Ping', value=f'```fix\n{round(websocket.total_seconds() * 1000)} ms\n```')
+        return await msg.edit(embed=embed)
+
+    @commands.command(name='uptime', aliases=['up', 'alive'])
+    async def uptime(self, ctx):
+        """Shows the uptime of the bot."""
+        embed = DefaultEmbed(ctx)
+        embed.title = f'{self.bot.user.name} Uptime'
+        embed.add_field(name='Current Uptime', value=f'```fix\n{self.bot.uptime.in_words()}\n```')
         return await ctx.send(embed=embed)
 
 
--- bot/cogs/tracker.py
+++ bot/cogs/tracker.py
@@ -146,7 +146,7 @@ class Tracker(commands.Cog):
             if who.id == ctx.author.id:
                 return await ctx.send(embed=ErrorEmbed(ctx, title='Stats Error!', description='You must sign up for'
                                                                                               'tracking to display'
-                                                                                              'stats.'))
+                                                                                              ' stats.'))
             else:
                 return await ctx.send(
                     embed=ErrorEmbed(ctx, title='Stats Error!', description=f'{who.name} has not signed up for hunt'
