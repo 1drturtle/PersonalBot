5d863df8903a96c8717eb2099615a27f151da1b3
Chris A <1drturtle@gmail.com>
2021-06-20 13:20:17 -0400

    tell users to opt-in

--- bot/cogs/tracker.py
+++ bot/cogs/tracker.py
@@ -145,8 +145,8 @@ class Tracker(commands.Cog):
         if not await self.redis.sismember(f'opted-{self.env}', str(who.id)):
             if who.id == ctx.author.id:
                 return await ctx.send(embed=ErrorEmbed(ctx, title='Stats Error!', description='You must sign up for'
-                                                                                              'tracking to display'
-                                                                                              ' stats.'))
+                                                                                              'tracking to display '
+                                                                                              'stats. See `tb!optin`'))
             else:
                 return await ctx.send(
                     embed=ErrorEmbed(ctx, title='Stats Error!', description=f'{who.name} has not signed up for hunt'
